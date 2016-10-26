b52ffc0cc39830b62d712051a26fedd6aa84e8a6
aj bredell <abredell@PC03.cis.com>
2016-10-26 10:15:31 -0500

    Added hello_world.py

---
+++ Hello_World.py
@@ -0,0 +1,7 @@
+# HelloWorld.py
+# A.J. Bredell
+# 10/26/2016
+
+"""Program that tells the world 'Hello'"""
+
+print("Hello, World!")
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Hello_World
 Test repository.
+CHANGELOG
+added helloworld.py 10/26/2016
